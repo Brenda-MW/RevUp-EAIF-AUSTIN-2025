3312f0a6feb51c393d00ddd4711371f4d531679c
Brenda Zhuang <139565107+Brenda-MW@users.noreply.github.com>
2025-02-23 13:56:44 -0500

    update ex5 and 6

--- Workshop/Ex6_QuantizeCompression/quantizationPostTraining.m
+++
@@ -1,7 +0,0 @@
-dlq = dlquantizer(netProjected,"ExecutionEnvironment","MATLAB"); 
-% use less data to calibrate
-dsLocal = subset(ds,1:30); 
-r = dlq.calibrate(dsLocal); 
-quantizedNet = dlq.quantize(); 
-
-ypred_quantized = predict(quantizedNet,XTest);
